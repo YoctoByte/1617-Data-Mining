a207ce93da07f8d7cfd08e3d9c87f0aa25381f2f
david <david.vtwout@gmail.com>
2016-08-30 22:09:11 +0200

    pep8-ed similarity.py

--- assignments/assignment 1/Python Toolbox/similarity.py
+++ assignments/assignment 1/Python Toolbox/similarity.py
@@ -3,7 +3,7 @@ from scipy.stats import zscore
 
 
 def similarity(X, Y, method):
-    '''
+    """
     SIMILARITY Computes similarity matrices
 
     Usage:
@@ -25,7 +25,8 @@ def similarity(X, Y, method):
         attribute binary according to x>median(x)
 
     Copyright, Morten Morup and Mikkel N. Schmidt
-    Technical University of Denmark '''
+    Technical University of Denmark
+    """
 
     X = np.mat(X)
     Y = np.mat(Y)
@@ -33,40 +34,41 @@ def similarity(X, Y, method):
     N2, M = np.shape(Y)
     
     method = method[:3].lower()
-    if method=='smc': # SMC
-        X,Y = binarize(X,Y);
+    if method == 'smc':  # SMC
+        X, Y = binarize(X, Y)
         sim = ((X*Y.T)+((1-X)*(1-Y).T))/M
-    elif method=='jac': # Jaccard
-        X,Y = binarize(X,Y);
+    elif method == 'jac':  # Jaccard
+        X,Y = binarize(X, Y)
         sim = (X*Y.T)/(M-(1-X)*(1-Y).T)        
-    elif method=='ext': # Extended Jaccard
+    elif method == 'ext':  # Extended Jaccard
         XYt = X*Y.T
-        sim = XYt / (np.log( np.exp(sum(np.power(X.T,2))).T * np.exp(sum(np.power(Y.T,2))) ) - XYt)
-    elif method=='cos': # Cosine
-        sim = (X*Y.T)/(np.sqrt(sum(np.power(X.T,2))).T * np.sqrt(sum(np.power(Y.T,2))))
-    elif method=='cor': # Correlation
-        X_ = zscore(X,axis=1,ddof=1)
-        Y_ = zscore(Y,axis=1,ddof=1)
+        sim = XYt / (np.log(np.exp(sum(np.power(X.T, 2))).T * np.exp(sum(np.power(Y.T, 2)))) - XYt)
+    elif method == 'cos':  # Cosine
+        sim = (X*Y.T)/(np.sqrt(sum(np.power(X.T, 2))).T * np.sqrt(sum(np.power(Y.T, 2))))
+    elif method == 'cor':  # Correlation
+        X_ = zscore(X, axis=1, ddof=1)
+        Y_ = zscore(Y, axis=1, ddof=1)
         sim = (X_*Y_.T)/(M-1)
+    else:
+        raise ValueError('Unknown method...')
     return sim
-        
-def binarize(X,Y=None):
-    ''' Force binary representation of the matrix, according to X>median(X) '''
-    if Y==None:
+
+
+def binarize(X, Y=None):
+    """ Force binary representation of the matrix, according to X>median(X) """
+    if Y == None:
         X = np.matrix(X)
-        Xmedians = np.ones((np.shape(X)[0],1)) * np.median(X,0)
-        Xflags = X>Xmedians
-        X[Xflags] = 1; X[~Xflags] = 0
+        Xmedians = np.ones((np.shape(X)[0], 1)) * np.median(X, 0)
+        Xflags = X > Xmedians
+        X[Xflags], X[~Xflags] = 1, 0
         return X
     else:
-        X = np.matrix(X); Y = np.matrix(Y);
-        XYmedian= np.median(np.bmat('X; Y'),0)
-        Xmedians = np.ones((np.shape(X)[0],1)) * XYmedian
-        Xflags = X>Xmedians
-        X[Xflags] = 1; X[~Xflags] = 0
-        Ymedians = np.ones((np.shape(Y)[0],1)) * XYmedian
-        Yflags = Y>Ymedians
-        Y[Yflags] = 1; Y[~Yflags] = 0
-        return [X,Y]
-        
-
+        X, Y = np.matrix(X), np.matrix(Y)
+        XYmedian = np.median(np.bmat('X; Y'), 0)
+        Xmedians = np.ones((np.shape(X)[0], 1)) * XYmedian
+        Xflags = X > Xmedians
+        X[Xflags], X[~Xflags] = 1, 0
+        Ymedians = np.ones((np.shape(Y)[0], 1)) * XYmedian
+        Yflags = Y > Ymedians
+        Y[Yflags], Y[~Yflags] = 1, 0
+        return [X, Y]
