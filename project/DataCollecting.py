7b0ba5d33c050d3e3964fe53751726fe4c2efc32
david <david.vtwout@gmail.com>
2016-09-02 10:12:33 +0200

    project htmlparser now has page class (not tested)

---
+++ project/DataCollecting.py
@@ -0,0 +1,176 @@
+from project import HTMLParser
+import requests
+from threading import Thread
+from time import sleep
+import json
+
+
+class WikipediaPage(HTMLParser.HTMLPage):
+    def get_info_table(self):
+        for table in self.get_elements('table'):
+            is_info_table = True
+            for tag_name in ['Identifiers', 'Properties']:
+                if tag_name not in str(table):
+                    is_info_table = False
+            if is_info_table:
+                return table
+
+
+class WikipediaParser:
+    def __init__(self):
+        self.terminate = False
+        self.data_filename = 'data/database_raw02.json'
+        self.urls_done_filename = 'data/tested_urls'
+        self.urls_to_do_filename = 'data/waitlist_urls'
+        self.urls_done = set()
+        self.urls_to_do = set()
+        self.data = dict()  # dict with molecules as keys. Molecule values are also dicts, with properties from the
+        # wikipedia info table as keys.
+        self.load_data()
+
+    def load_data(self):
+        with open(self.urls_done_filename) as file:
+            for line in file:
+                self.urls_done.add(line.strip())
+        with open(self.urls_to_do_filename) as file:
+            for line in file:
+                self.urls_to_do.add(line.strip())
+        with open(self.data_filename) as json_file:
+            self.data = json.loads(json_file.read())
+        print('number of molecules: ' + str(len(self.data)))
+        print('length of wait-list: ' + str(len(self.urls_to_do)))
+        print('length of tested urls: ' + str(len(self.urls_done)))
+
+    @staticmethod
+    def get_initial_urls():
+        initial_urls_filename = 'data/initial_urls'
+
+        wiki_domain = 'https://en.wikipedia.org'
+        data_urls = ['/wiki/Dictionary_of_chemical_formulas',
+                     '/wiki/List_of_biomolecules',
+                     '/wiki/List_of_inorganic_compounds']
+
+        initial_urls = set()
+        for data_url in data_urls:
+            page = WikipediaPage(url=wiki_domain+data_url)
+            for link in page.get_links():
+                if link[:5] == '/wiki':
+                    initial_urls.add(wiki_domain+link)
+        with open(initial_urls_filename, 'w') as output_file:
+            for url in initial_urls:
+                output_file.write(url + '\n')
+
+    def page_parser_thread(self):
+        while not self.terminate:
+            current_url = self.urls_to_do.pop()
+            if current_url in self.urls_done:
+                continue
+            self.urls_done.add(current_url)
+            try:
+                page_string = requests.get(current_url).content.decode('utf-8')
+                page = WikipediaPage(html_string=page_string)
+            except ValueError:
+                continue
+            info_table = page.get_info_table()
+            if info_table:
+                table_data = dict()
+                for key, value in parse_info_table(info_table):
+                    table_data[key] = value
+                molecule_name = current_url.split('/')[-1]
+                self.data[molecule_name] = table_data
+                with open('data/downloads/' + molecule_name + '.html', 'w') as file:
+                    file.write(page_string)
+
+                wiki_domain = 'https://en.wikipedia.org'
+                for link in page.get_links():
+                    if link[:5] == '/wiki':
+                        if wiki_domain + link not in self.urls_done:
+                            self.urls_to_do.add(wiki_domain+link)
+                print(current_url.split('/')[-1] + ' parsed to database.')
+
+    def file_update_thread(self):
+        while not self.terminate:
+            sleep(30)
+            with open(self.urls_done_filename, 'w') as file:
+                file_string = ''
+                for url in self.urls_done.copy():
+                    file_string += url + '\n'
+                file.write(file_string)
+            with open(self.urls_to_do_filename, 'w') as file:
+                file_string = ''
+                for url in self.urls_to_do.copy():
+                    file_string += url + '\n'
+                file.write(file_string)
+            with open(self.data_filename, 'w') as json_file:
+                json_file.write(json.dumps(self.data, separators=(',', ':'), sort_keys=True, indent=4))
+            print('number of molecules: ' + str(len(self.data)))
+            print('length of waitlist: ' + str(len(self.urls_to_do)))
+            print('length of tested urls: ' + str(len(self.urls_done)))
+
+    def input_thread(self):
+        while not self.terminate:
+            inp = input('Enter command: ')
+            if inp.lower() in ['quit', 'q']:
+                self.terminate = True
+
+    def run(self):
+        t_update = Thread(target=self.file_update_thread)
+        t_update.start()
+        t_input = Thread(target=self.input_thread)
+        t_input.start()
+        t_parsers = list()
+        for _ in range(20):
+            t_parser = Thread(target=self.page_parser_thread)
+            t_parser.start()
+            t_parsers.append(t_parser)
+
+        t_update.join()
+        t_input.join()
+        for t in t_parsers:
+            t.join()
+
+
+def parse_info_table(info_table):
+    for row in info_table:
+        key = None
+        value = None
+        row.remove('sup')
+        if len(row) == 2:
+            key = str(row[0])
+            try:
+                list_element = row[1].get_elements('ul')
+            except AttributeError:
+                continue
+            if list_element:
+                value = list()
+                for item in list_element[0]:
+                    value.append(str(item))
+            else:
+                value = str(row[1])
+        elif len(row) == 1:
+            list_element = row.get_elements('ul')
+            if list_element:
+                value = list()
+                for item in list_element[0]:
+                    value.append(str(item))
+                key = str(row).replace(str(list_element[0]), '')
+        if key and value:
+            yield key, value
+
+
+def download_page(url):
+    page = requests.get(url)
+    html_string = page.content.decode('utf-8')
+    with open('data/downloads/' + url.split('/')[-1] + '.html', 'w') as output_file:
+        output_file.write(html_string)
+
+
+# download_page('https://en.wikipedia.org/wiki/Adenosine_triphosphate')
+# page = htmlparser.parse_from_file('data/downloads/Methane.html')
+# info_table = get_info_table(page)
+# for key, value in parse_info_table(info_table):
+#     print('--------')
+#     print(key, '-', value)
+# get_initial_urls()
+wikiparser = WikipediaParser()
+wikiparser.run()
--- project/HTMLParser.py
+++ project/HTMLParser.py
@@ -13,93 +13,7 @@ non_closing_tag_names = ['area', 'base', 'br', 'col', 'command',
 # \u2013 : longer minus sign
 
 
-def parse_from_url(url):
-    page = requests.get(url)
-    html_string = page.content.decode('utf-8')
-    return parse_from_string(html_string)
-
-
-def parse_from_file(filename):
-    with open(filename, 'r') as file:
-        html_string = file.read()
-        file.close()
-    return parse_from_string(html_string)
-
-
-def parse_from_string(html_string):
-    depth = 0
-    html_page = Element()
-    for token in tokenize(html_string):
-        if token[0] == '<':  # Token is a tag or comment
-            if token[1] == '!':  # Token is a comment
-                continue
-            elif token[1] == '/':  # Token is an end-tag
-                if token[2:-1] not in non_closing_tag_names:
-                    depth -= 1
-            else:  # Token is a start-tag
-                element = parse_tag_token(token)
-                html_page.add(element, depth)
-                if element.name not in non_closing_tag_names:
-                    depth += 1
-        else:  # Token is text
-            html_page.add(token, depth)
-    return html_page
-
-
-def tokenize(html_string):
-    """
-    :param html_string: The HTML string to be tokenized.
-    :return: Yield tag tokens and plain text tokens until end of string. Yielded tag tokens do include the brackets
-    """
-
-    # Compress the string to a single line:
-    new_string = ''
-    for line in html_string.split('\n'):
-        new_string += line.strip()
-    html_string = new_string
-
-    # Start yielding tokens:
-    pos_left = html_string.find('<')
-    text_token = html_string[0:pos_left]
-    if text_token:
-        yield text_token.replace('&#160;', ' ').replace('&#160', ' ')
-    while True:
-        pos_right = html_string.find('>', pos_left)
-        tag_token = html_string[pos_left:pos_right+1]
-        yield tag_token
-        if pos_right == -1:
-            raise ValueError('Error during tokenizing of the html string.')
-        pos_left = html_string.find('<', pos_right)
-        if pos_left == -1:  # If end of string
-            text_token = html_string[pos_right+1:]
-            if text_token:
-                yield text_token.replace('&#160;', ' ').replace('&#160', ' ')
-            break
-        text_token = html_string[pos_right+1:pos_left]
-        if text_token:
-            yield text_token.replace('&#160;', ' ').replace('&#160', ' ')
-
-
-def parse_tag_token(tag_token):
-    # tokenize the token even further:
-    tag_token = tag_token.lstrip('<').rstrip('>')
-    attribute_strings = tag_token.split(' ')
-    tag_name = attribute_strings.pop(0)
-
-    # Parse the attributes of the tag:
-    attributes_dict = dict()
-    for attribute in attribute_strings:
-        if attribute in ['/']:
-            continue
-        try:
-            key, value = attribute.split('=', 1)
-        except ValueError:
-            key, value = attribute, ''
-        attributes_dict[key] = value.strip('"')
-    return Element(name=tag_name, attributes=attributes_dict)
-
-
-class Element:
+class HTMLElement:
     def __init__(self, name='', attributes=None, content=None):
         self.name = name
         self.attributes = dict()
@@ -113,20 +27,19 @@ class Element:
         if depth < 0:
             raise ValueError('depth too high')
         if depth == 0:
-            if isinstance(content, Element) or isinstance(content, str):
+            if isinstance(content, HTMLElement) or isinstance(content, str):
                 self.content.append(content)
             else:
                 raise ValueError('Added content should be one of the following types: string, Element.')
         else:
-            # todo: rewrite this stupid looking code:
             for item in reversed(self.content):
-                if isinstance(item, Element):
+                if isinstance(item, HTMLElement):
                     item.add(content, depth-1)
                     break
 
     def get_elements(self, name, recursive=True):
         elements = list()
-        for content in (item for item in self.content if isinstance(item, Element)):
+        for content in (item for item in self.content if isinstance(item, HTMLElement)):
             if content.name == name:
                 elements.append(content)
             if recursive:
@@ -134,11 +47,10 @@ class Element:
         return elements
 
     def remove(self, name=None, attribute=None, recursive=True):
-        # First some pre-processing of the arguments:
         if not name and not attribute:
             return
 
-        # The function that check whether or not the attribute is in the content item:
+        # The function that checks whether or not the attribute is in the content item:
         def attr_match(cont_attrs):
             if not attribute:
                 return True
@@ -167,7 +79,7 @@ class Element:
                     return False
 
         for i, content in enumerate(self.content):
-            if isinstance(content, Element):
+            if isinstance(content, HTMLElement):
                 if (content.name == name or not name) and attr_match(content.attributes):
                     del self.content[i]
                     continue
@@ -223,7 +135,7 @@ class Element:
         return '<' + self.name + attr_string + '>\n' + content_string + end_tag
 
     def copy(self):
-        element_copy = Element()
+        element_copy = HTMLElement()
         for item in self.content:
             if not isinstance(item, str):
                 element_copy.content.append(item.copy())
@@ -237,3 +149,95 @@ class Element:
             if 'href' in link.attributes:
                 links.append(link.attributes['href'])
         return links
+
+
+class HTMLPage(HTMLElement):
+    def __init__(self, filename=None, url=None, html_string=None):
+        HTMLElement.__init__(self)
+        if html_string:
+            self.parse_from_string(html_string)
+        elif filename:
+            self.parse_from_file(filename)
+        elif url:
+            self.parse_from_url(url)
+
+    def parse_from_url(self, url):
+        page = requests.get(url)
+        html_string = page.content.decode('utf-8')
+        self.parse_from_string(html_string)
+
+    def parse_from_file(self, filename):
+        with open(filename, 'r') as file:
+            html_string = file.read()
+            file.close()
+        self.parse_from_string(html_string)
+
+    def parse_from_string(self, html_string):
+        depth = 0
+        for token in self.tokenize(html_string):
+            if token[0] == '<':  # Token is a tag or comment
+                if token[1] == '!':  # Token is a comment
+                    continue
+                elif token[1] == '/':  # Token is an end-tag
+                    if token[2:-1] not in non_closing_tag_names:
+                        depth -= 1
+                else:  # Token is a start-tag
+                    element = self.parse_tag_token(token)
+                    self.content.add(element, depth)
+                    if element.name not in non_closing_tag_names:
+                        depth += 1
+            else:  # Token is text
+                self.content.add(token, depth)
+
+    @staticmethod
+    def tokenize(html_string):
+        """
+        :param html_string: The HTML string to be tokenized.
+        :return: Yield tag tokens and plain text tokens until end of string. Yielded tag tokens do include the brackets
+        """
+
+        # Compress the string to a single line:
+        new_string = ''
+        for line in html_string.split('\n'):
+            new_string += line.strip()
+        html_string = new_string
+
+        # Start yielding tokens:
+        pos_left = html_string.find('<')
+        text_token = html_string[0:pos_left]
+        if text_token:
+            yield text_token.replace('&#160;', ' ').replace('&#160', ' ')
+        while True:
+            pos_right = html_string.find('>', pos_left)
+            tag_token = html_string[pos_left:pos_right+1]
+            yield tag_token
+            if pos_right == -1:
+                raise ValueError('Error during tokenizing of the html string.')
+            pos_left = html_string.find('<', pos_right)
+            if pos_left == -1:  # If end of string
+                text_token = html_string[pos_right+1:]
+                if text_token:
+                    yield text_token.replace('&#160;', ' ').replace('&#160', ' ')
+                break
+            text_token = html_string[pos_right+1:pos_left]
+            if text_token:
+                yield text_token.replace('&#160;', ' ').replace('&#160', ' ')
+
+    @staticmethod
+    def parse_tag_token(tag_token):
+        # tokenize the token even further:
+        tag_token = tag_token.lstrip('<').rstrip('>')
+        attribute_strings = tag_token.split(' ')
+        tag_name = attribute_strings.pop(0)
+
+        # Parse the attributes of the tag:
+        attributes_dict = dict()
+        for attribute in attribute_strings:
+            if attribute in ['/']:
+                continue
+            try:
+                key, value = attribute.split('=', 1)
+            except ValueError:
+                key, value = attribute, ''
+            attributes_dict[key] = value.strip('"')
+        return HTMLElement(name=tag_name, attributes=attributes_dict)
