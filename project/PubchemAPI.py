05874e040796f21102880064a6748c09cbe44be0
david <david.vtwout@gmail.com>
2016-09-08 21:58:15 +0200

    pubchem api ready

--- project/PubchemAPI.py
+++ project/PubchemAPI.py
@@ -1,21 +1,50 @@
 import requests
+import json
 
 
-api_url_root = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/'
-input_specification = 'compound/'
-operation_specification = '294764/'
-output_specification = 'JSON'
+PUBCHEM_ROOT_DIRECTORY = 'data/pubchem/'
 
 
-def download_page(url):
-    page = requests.get(url)
-    html_string = page.content.decode('utf-8')
-    print(html_string)
-    with open('data/downloads/' + url.split('/')[-1] + '.html', 'w') as output_file:
-        output_file.write(html_string)
+class PubChemPage:
+    def __init__(self, pubchem_id):
+        self.pubchem_id = str(pubchem_id)
+        url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/' + self.pubchem_id + '/JSON'
+        json_request = requests.get(url)
+        self.json_string = json_request.content.decode('utf-8')
+        with open(PUBCHEM_ROOT_DIRECTORY+'json_raw/'+self.pubchem_id+'.json', 'w') as json_file:
+            json_file.write(self.json_string)
+        self.json_data = json.loads(self.json_string)
+        self.data, self.descriptions = self._to_dict()
 
+    def _to_dict(self):
+        data = dict()
+        descriptions = dict()
+        record = self.json_data['Record']
+        main_section = record['Section']
 
-# api_url = api_url_root+input_specification+operation_specification+output_specification
-download_page('https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/294765/JSON')
+        def extract_values(section):
+            for item in section:
+                if 'Section' in item:
+                    extract_values(item['Section'])
+                elif 'Information' in item:
+                    heading, information = item['TOCHeading'], item['Information']
+                    try:
+                        description = item['Description']
+                    except KeyError:
+                        description = None
+                    data[heading] = information
+                    descriptions[heading] = description
 
-# <input specification>/<operation specification>/[<output specification>][?<operation_options>]
+        extract_values(main_section)
+
+        return data, descriptions
+
+    def save(self, filename=''):
+        if not filename:
+            filename = PUBCHEM_ROOT_DIRECTORY+'json_stage_1/'+self.pubchem_id+'.json'
+        with open(filename, 'w') as json_file:
+            json_file.write(json.dumps(self.data, separators=(',', ':'), sort_keys=True, indent=4))
+
+    def __iter__(self):
+        for attr, val in self.data.items():
+            yield attr, val
